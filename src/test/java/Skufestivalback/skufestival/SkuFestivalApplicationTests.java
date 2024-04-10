7ad597dd75def6ccae609c1c08395e0b2faa5bf6
kssosoy <qws1566@naver.com>
2024-04-10 14:30:19 +0900

    set up

---
+++ src/test/java/Skufestivalback/skufestival/SkuFestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package Skufestivalback.skufestival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuFestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
