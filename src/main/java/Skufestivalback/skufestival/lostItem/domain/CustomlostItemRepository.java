de3994a52e1ab7c3c4ca9b2dab3ba6881ca00e30
Hyuneun <hyuneun@skuniv.ac.kr>
2024-04-30 12:57:32 +0900

    :rocket: Notice

--- src/main/java/Skufestivalback/skufestival/lostItem/domain/CustomlostItemRepository.java
+++
@@ -1,9 +0,0 @@
-package Skufestivalback.skufestival.lostItem.domain;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomlostItemRepository {
-
-    lostItem findByIdOrElseThrow(Long id);
-}
