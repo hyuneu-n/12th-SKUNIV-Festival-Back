365f2bd3b79dfef82cc83de4d9adcbbf4c903862
Hyuneun <hyuneun@skuniv.ac.kr>
2024-05-05 04:09:26 +0900

    delete paging feature

--- src/main/java/Skufestivalback/skufestival/lostItem/domain/lostItemRepository.java
+++ src/main/java/Skufestivalback/skufestival/lostItem/domain/lostItemRepository.java
@@ -2,7 +2,6 @@ package Skufestivalback.skufestival.lostItem.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +9,6 @@ import java.util.List;
 @Repository
 public interface lostItemRepository extends JpaRepository<lostItem, Long>, CustomlostItemRepository {
 
-    @Query(nativeQuery = true, value = "SELECT * FROM lost_item WHERE id < :lastId ORDER BY id DESC LIMIT :pageSize")
-    List<lostItem> find(@Param("lastId") Long lastId, @Param("pageSize") int pageSize);
-
+    @Query(nativeQuery = true, value = "SELECT * FROM lost_item ORDER BY id DESC")
+    List<lostItem> findAllSortedByIdDesc();
 }
