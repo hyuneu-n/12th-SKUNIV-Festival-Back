bd5429477e7afc3421074ff76b0dc868d5d15c5e
Hyuneun <hyuneun@skuniv.ac.kr>
2024-05-07 03:16:49 +0900

    :bug:

--- src/main/java/Skufestivalback/skufestival/lostItem/service/PostlostItemService.java
+++ src/main/java/Skufestivalback/skufestival/lostItem/service/PostlostItemService.java
@@ -13,7 +13,7 @@ public class PostlostItemService {
     private final lostItemRepository lostItemRepository;
 
     @Transactional
-    public void doService(String lostItemName, String lostItemImagePath, String lostDate, String lostLocation, boolean lost) {
+    public void doService(String lostItemName, String lostItemImagePath, String lostDate, String lostLocation, Boolean lost) {
         lostItem lostitem = new lostItem(lostItemName, lostItemImagePath, lostDate, lostLocation, lost);
         lostItemRepository.save(lostitem);
     }
